6171ce72168bf5f98582c2c54c6ea247a5f5c70e
João Batista da Rocha Neto <framework25@localiza.com>
2022-05-11 08:57:41 -0300

    Aplicação de ViewChild e ViewChildren

--- src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
+++ src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { ProdutoDetalheComponent } from '../componentes/produto-card-detalhe-component';
+import { ProdutoCountComponent } from '../componentes/produto-count-component';
 import { Produto } from '../models/produto';
 
 @Component({
   selector: 'app-produto-dashboard',
   templateUrl: './produto-dashboard.component.html'
 })
-export class ProdutoDashboardComponent implements OnInit {
+export class ProdutoDashboardComponent implements OnInit, AfterViewInit{
 
   produtos: Produto[]
   
+  @ViewChild(ProdutoCountComponent, { static: false }) contador: ProdutoCountComponent;
+  @ViewChild('teste', { static: false }) mensagemTela : ElementRef;
+  @ViewChildren(ProdutoDetalheComponent) botoes: QueryList<ProdutoDetalheComponent>;
+
   constructor() { }
 
   ngOnInit() {
@@ -54,6 +61,22 @@ export class ProdutoDashboardComponent implements OnInit {
       valor: 600,
       imagem: 'headset.jpg'
     }];
+  }
+
+  ngAfterViewInit(): void {
+    console.log('Objeto do Contador: ', this.contador.produtos);
+
+    let clickText: Observable<any> = fromEvent(this.mensagemTela.nativeElement, 'click');
+
+    clickText.subscribe(() => {
+      alert('Clicou no texto!');
+      return;
+    });
+
+    console.log(this.botoes);
+    this.botoes.forEach(p => {
+      console.log(p.produto);
+    });
   }
 
   mudarStatus(event: Produto){
