3c42f85bf5fe2c7dacf4038277749f6e42e5bae5
João Batista da Rocha Neto <framework25@localiza.com>
2022-11-07 22:45:00 -0300

    Criação da Store para armazenamento de dados; Filtro das tasks

--- src/app/demos/todo-list/components/tasks-finalizadas/tasks-finalizadas.component.ts
+++ src/app/demos/todo-list/components/tasks-finalizadas/tasks-finalizadas.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
+import { map } from 'rxjs/operators';
 import { TasksService } from "../../todo.service";
+import { Store } from "../../todo.store";
 
 @Component({
     selector: 'tasks-finalizadas',
@@ -10,9 +12,12 @@ export class TasksFinalizadasComponent implements OnInit {
 
     finalizados$: Observable<any[]>;
 
-    constructor(private tasksService: TasksService) { }
+    constructor(private tasksService: TasksService, private store: Store) { }
   
     ngOnInit() {
-      this.finalizados$ = this.tasksService.getTodoList$;      
+      this.finalizados$ = this.store.getTodoList()
+      .pipe(
+        map(todolist => todolist.filter(task => task.finalizado))
+      );      
     }
 }
--- src/app/demos/todo-list/todo.service.ts
+++ src/app/demos/todo-list/todo.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
-import { Task } from './task';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+import { Task } from "./task";
+import { Store } from "./todo.store";
 
 @Injectable()
 export class TasksService {
+  constructor(private http: HttpClient, private store: Store) {}
 
-    constructor(private http: HttpClient) { }
-
-    getTodoList$: Observable<Task[]> = this.http
-        .get<Task[]>('http://localhost:3000/todolist');
-
+  getTodoList$: Observable<Task[]> = this.http
+    .get<Task[]>("http://localhost:3000/todolist")
+    .pipe(tap((next) => this.store.set("todolist", next)));
 }
